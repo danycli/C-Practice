493505911cb344ef824576720e35ce597e2f638c
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-08-26 13:02:41 +0500

    Create libraries.c

---
+++ libraries.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float i = 2.4;
+    printf("%f\n",ceil(i));
+    printf("%f\n",floor(i));
+    printf("%f\n",sqrt(i));
+    printf("%f\n",pow(i,2));
+return 0;
+}
