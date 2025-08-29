d071e85900cbbe08049e5877474b3cf697eb0243
SAHIBZADA-DANY <angelgaming331@gmail.com>
2025-08-29 16:01:36 +0500

    Create stringg.c

---
+++ stringg.c
@@ -0,0 +1,18 @@
+ int len = strlen(needle)+1;
+    int check = 0;
+    char match [len];
+    for(int i =0; i<len-1; i++){
+        match[i] = haystack[i];
+    }
+        for(int i = 0; i<len-1; i++){
+            if(match[i] == needle[i]){
+                check = 0;
+            }else{
+                check = 1;
+            }
+        }
+        if(check == 0){
+            return 0;
+        }else{
+            return -1;
+        }
